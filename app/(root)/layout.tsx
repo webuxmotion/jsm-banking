fb86674fb7b34c7bdd0356abb6c10bc765c0db70
webuxmotion <pereverziev.andrii@gmail.com>
2024-09-04 19:17:40 +0300

    Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,21 @@
+import Sidebar from '@/components/Sidebar';
 import React from 'react'
 
 const RootLayout = ({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const loggedIn = {
+    firstName: "Andrii",
+    lastName: "Pereverziev"
+  }
+
   return (
-    <div>
-        SIDEBAR
-        {children}
-    </div>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
+    </main>
   )
 }
 
--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -3,15 +3,14 @@
 import React from 'react'
 import CountUp from 'react-countup'
 
-
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <CountUp 
-        duration={2.75}
-        end={amount} 
-        decimal=","
-        decimals={2}
-        prefix="$"
+    <CountUp
+      duration={1}
+      end={amount}
+      decimal=","
+      decimals={2}
+      prefix="$"
     />
   )
 }
